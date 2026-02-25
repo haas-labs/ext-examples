6eb205ee7c51618eec77d2e72b0821a4555f5e85
andriyk-hacken <a.kashcheyev@hacken.io>
2026-02-25 20:32:10 +0200

    Fix detector get

--- ext-cli/ext-contract-detectors.sh
+++ ext-cli/ext-contract-detectors.sh
@@ -1,12 +1,24 @@
-# TID - alien tenantID only works with Admin JWT
-CID=${1:-0}
+#!/bin/bash
+CWD=`echo $(dirname $(readlink -f $0))`
+
+# TID - alient tenantID only works with Admin JWT
+CID=${1:-3109}
+#TAGS=${TAGS:-COMPLIANCE}
+TAGS=${2:-}
 
 SERVICE_URI=${SERVICE_URI:-https://localhost:8080/api/v1}
 ACCESS_TOKEN=${ACCESS_TOKEN-`cat ACCESS_TOKEN`}
 
+if [ "$TAGS" != "" ]; then
+  Q_TAGS=" and tags='${TAGS}'"
+fi
+
+
 >&2 echo "CID=$CID"
 
-DATA_JSON="{\"size\":100,\"trackTotal\":false,\"where\":\"contractId = $CID OR contractId = $CID\"}"
+DATA_JSON="{\"where\":\"contractId = $CID${Q_TAGS}\",\"trackTotal\":false,\"from\":0,\"size\":10,\"sort\":[{\"field\":\"createdAt\",\"order\":\"Desc\"}]}"
+
+>&2 echo "DATA_JSON=$DATA_JSON"
 
 curl -S -s -D /dev/stderr \
    -X POST \
--- ext-cli/ext-detector.sh
+++ ext-cli/ext-detector.sh
@@ -1,19 +1,17 @@
+#!/bin/bash
+CWD=`echo $(dirname $(readlink -f $0))`
+
 # TID - alient tenantID only works with Admin JWT
-CID=${1:-3109}
-TAGS=${TAGS:-COMPLIANCE}
+DID=${1:-3109}
 
 SERVICE_URI=${SERVICE_URI:-https://localhost:8080/api/v1}
 ACCESS_TOKEN=${ACCESS_TOKEN-`cat ACCESS_TOKEN`}
 
->&2 echo "CID=$CID"
-
-DATA_JSON="{\"where\":\"contractId = $CID and tags='${TAGS}'\",\"trackTotal\":false,\"from\":0,\"size\":10,\"sort\":[{\"field\":\"createdAt\",\"order\":\"Desc\"}]}"
-
->&2 echo "DATA_JSON=$DATA_JSON"
+>&2 echo "DID=$DID"
 
 curl -S -s -D /dev/stderr \
-   -X POST \
+   -X GET \
    -H 'Content-Type: application/json' \
    -H "Authorization: Bearer $ACCESS_TOKEN" \
    --data "$DATA_JSON" \
-   $SERVICE_URI/detector/search
+   $SERVICE_URI/detector/${DID}
